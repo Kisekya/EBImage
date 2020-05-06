a7283e476d6f76fa9d32b086f3f79131f88a9b6e
Kisekya <coralie.muller.mazin@gmail.com>
2020-05-06 16:56:26 +0200

    test viewer IDE6

--- inst/htmlwidgets/lib/viewer/viewer.js
+++ inst/htmlwidgets/lib/viewer/viewer.js
@@ -203,7 +203,7 @@ function Viewer(parent){
 
 		currentFrame = frame;
 		viewer.updateStatusField("Frame", currentFrame+'/'+numberOfFrames);
-	        Shiny.setInputValue(ns("currentFrame"), currentFrame);
+	        Shiny.setInputValue(session$ns("currentFrame"), currentFrame);
 	    
 		// button locking
 		buttons['first'].disable(currentFrame==1);
@@ -378,7 +378,7 @@ function Viewer(parent){
 
 	    image.style.cursor = cursor;
 	    previousMousePosition = getMouseXY(event);
-	    Shiny.setInputValue(ns("pixelPosition"), viewer.getPixelPosition(event));
+	    Shiny.setInputValue(session$ns("pixelPosition"), viewer.getPixelPosition(event));
 	    image.onmousemove = viewer.dragImage;
 	}
 
@@ -548,8 +548,8 @@ function Viewer(parent){
 	viewer.setFrame();
 	image.onload = viewer.resetCanvas();
 
-	Shiny.setInputValue(ns("imgWidth"), originalWidth);
-	Shiny.setInputValue(ns("imgHeight"), originalHeight);
+	Shiny.setInputValue(session$ns("imgWidth"), originalWidth);
+	Shiny.setInputValue(session$ns("imgHeight"), originalHeight);
 	viewer.updateStatusField("Image", originalWidth+'x'+originalHeight);
       
   }
