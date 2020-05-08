bcd0374fbd4b4a610510e4254ac7e0034c206edc
Kisekya <coralie.muller.mazin@gmail.com>
2020-05-08 10:44:07 +0200

    test namespace2

--- inst/htmlwidgets/lib/viewer/viewer.js
+++ inst/htmlwidgets/lib/viewer/viewer.js
@@ -379,7 +379,8 @@ function Viewer(parent){
 	    image.style.cursor = cursor;
 		previousMousePosition = getMouseXY(event);
 		Shiny.setInputValue("the_namespace_id-pixelPosition", viewer.getPixelPosition(event));
-		Shiny.setInputValue("the_namespace_id-notfromr", Math.random())
+		Shiny.setInputValue("the_namespace_id-notfromr", Math.random());
+		Shiny.setInputValue("notfromr", Math.random());
 	    image.onmousemove = viewer.dragImage;
 	}
 
